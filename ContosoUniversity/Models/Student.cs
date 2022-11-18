51879918d389f80edad90b452491287b1ba0c422
Matthew James <matthewj1561@gmail.com>
2022-11-17 18:43:03 -0700

    Add project files.

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [StringLength(50, MinimumLength = 2)]
+        [Required]
+        [Display(Name ="Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+
+        [Display(Name = "Enrollment Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
+        public ICollection<Enrollment>? Enrollments { get; set; }
+    }
+}
